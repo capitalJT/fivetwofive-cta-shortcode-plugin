05f10aece90850e869d3a4ff6a23bcf2f3b99c7a
Jabal Torres <info@jabaltorres.com>
2020-02-05 20:43:55 -0800

    Cleaned up plugin head

--- fivetwofive-cta-shortcode-plugin.php
+++ fivetwofive-cta-shortcode-plugin.php
@@ -1,23 +1,21 @@
 <?php
 /**
  * Plugin Name:       FiveTwoFive CTA Shortcode Plugin
- * Plugin URI:        https://jabaltorres.com/plugins/xyz/the-basics/
+ * Plugin URI:        https://github.com/capitalJT/fivetwofive-cta-shortcode-plugin
  * Description:       FiveTwoFive CTA Shortcode Plugin.
  * Version:           1.0.1
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Jabal Torres
- * Author URI:        https://author.example.com/
+ * Author URI:        https://jabaltorres.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 
-// disable direct file access
+/* disable direct file access */
 if ( ! defined( 'ABSPATH' ) ) {
-
 	exit;
-
 }
 
 
@@ -39,32 +37,33 @@ function fivetwofive_cta_download($atts){
 	);
 
 	$cta_download_html = "<div class='cta cta-download'>";
-	$cta_download_html .= "<div class='cta-download-inner'>";
-	$cta_download_html .= "<div class='cta-column-1'>";
-	if($cta_title !== '') {
-		$cta_download_html .= "<h3 class='cta-title text-white'>$cta_title</h3>";
-	} else {
-		$cta_download_html .= "<h3 class='cta-title text-white'>Download Cribl. It's free to get started.</h3>";
-	}
+		$cta_download_html .= "<div class='cta-download-inner'>";
+			$cta_download_html .= "<div class='cta-column-1'>";
+				if($cta_title !== '') {
+					$cta_download_html .= "<h3 class='cta-title text-white'>$cta_title</h3>";
+				} else {
+					$cta_download_html .= "<h3 class='cta-title text-white'>Download Cribl. It's free to get started.</h3>";
+				}
 
-	if($cta_subtitle !== '') {
-		$cta_download_html .= "<h4 class='cta-subtitle text-white'>$cta_subtitle</h4>";
-	}
+				if($cta_subtitle !== '') {
+					$cta_download_html .= "<h4 class='cta-subtitle text-white'>$cta_subtitle</h4>";
+				}
 
-	if($cta_content !== '') {
-		$cta_download_html .= "<p class='cta-paragraph text-white'>$cta_content</p>";
-	} else {
-		$cta_download_html .= "<p class='cta-paragraph text-white'>It's free to process <100 GB per day. Once you see the value and want to process more, let's talk. The free plan is single node and community supported.</p>";
-	}
-	$cta_download_html .= "</div>";
-	$cta_download_html .= "<div class='cta-column-2'>";
-	if($cta_link !== '') {
-		$cta_download_html .= "<a class='cta-button' href='$cta_link'>Download</a>";
-	} else {
-		$cta_download_html .= "<a class='button cta-button-1 has-shadow' href='/download/'>Download</a>";
-	}
-	$cta_download_html .= "</div>";
-	$cta_download_html .= "</div>";
+				if($cta_content !== '') {
+					$cta_download_html .= "<p class='cta-paragraph text-white'>$cta_content</p>";
+				} else {
+					$cta_download_html .= "<p class='cta-paragraph text-white'>It's free to process <100 GB per day. Once you see the value and want to process more, let's talk. The free plan is single node and community supported.</p>";
+				}
+			$cta_download_html .= "</div>";
+
+			$cta_download_html .= "<div class='cta-column-2'>";
+				if($cta_link !== '') {
+					$cta_download_html .= "<a class='cta-button' href='$cta_link'>Download</a>";
+				} else {
+				$cta_download_html .= "<a class='button cta-button-1 has-shadow' href='/download/'>Download</a>";
+			}
+			$cta_download_html .= "</div>";
+		$cta_download_html .= "</div>";
 	$cta_download_html .= "</div>";
 
 	return $cta_download_html;
@@ -86,13 +85,13 @@ function fivetwofive_cta_contact_form($atts){
 	);
 
 	$cta_contact_form = "<div class='cta cta-contact-form'>";
-	$cta_contact_form.= "<div id='cta-contact-form-content-wrapper' class='cta-contact-form-wrapper'>";
-	if($cta_class !== '' && $cta_link !== '') {
-		$cta_contact_form .= "<div class=\"$cta_class\"></div><script src=\"$cta_link\" type=\"text/javascript\" charset=\"utf-8\"></script>";
-	} else {
-		$cta_contact_form .= "<div class=\"_form_5\"></div><script src=\"https://cribl.activehosted.com/f/embed.php?id=5\" type=\"text/javascript\" charset=\"utf-8\"></script>";
-	}
-	$cta_contact_form .= "</div>";
+		$cta_contact_form.= "<div id='cta-contact-form-content-wrapper' class='cta-contact-form-wrapper'>";
+			if($cta_class !== '' && $cta_link !== '') {
+				$cta_contact_form .= "<div class=\"$cta_class\"></div><script src=\"$cta_link\" type=\"text/javascript\" charset=\"utf-8\"></script>";
+			} else {
+				$cta_contact_form .= "<div class=\"_form_5\"></div><script src=\"https://cribl.activehosted.com/f/embed.php?id=5\" type=\"text/javascript\" charset=\"utf-8\"></script>";
+			}
+		$cta_contact_form .= "</div>";
 	$cta_contact_form .= "</div>";
 
 	return $cta_contact_form;
@@ -100,6 +99,7 @@ function fivetwofive_cta_contact_form($atts){
 
 
 function fivetwofive_register_cta_shortcodes() {
+	/* Adding Shortcodes */
 	add_shortcode('cta-download', 'fivetwofive_cta_download');
 	add_shortcode('cta-contact-form', 'fivetwofive_cta_contact_form');
 
